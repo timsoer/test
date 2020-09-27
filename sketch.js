45c746612884e690f1416b6ba5535ad5c2c2bb78
timsoer <timsoer@yahoo.de>
2020-09-27 22:24:05 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var x, y;
 function setup() {
   //createCanvas(720, 400);
   //pixelDensity(1);
-  createCanvas(displayWidth - 200 , displayHeight -200);
+  createCanvas(displayWidth  , displayHeight );
   // Starts in the middle
   x = width / 2;
   y = height;
