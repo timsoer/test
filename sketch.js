534e6a5c6f5cbc99693fe0a516e108bb1acfcde8
timsoer <timsoer@yahoo.de>
2020-09-29 20:10:09 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,6 +23,7 @@ function draw() {
   ellipse(x, y, 42, 42);
   
   // Jiggling randomly on the horizontal axis
+  x = random(width, 1);
   x = x + random(-1, 1);
   // Moving up at a constant speed
   y = y - 1;
